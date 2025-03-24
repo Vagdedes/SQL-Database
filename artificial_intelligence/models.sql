4d7617c40bb39fe77b2ed1e22f7cbc0757aaab28
Evangelos <vaggdedes@gmail.com>
2025-03-24 19:52:55 +0200

    Updated AI tables

--- artificial_intelligence/models.sql
+++ artificial_intelligence/models.sql
@@ -2,14 +2,12 @@ create table models
 (
     id                        int auto_increment
         primary key,
-    type                      int           null,
     family                    int           null,
     request_url               varchar(128)  null,
-    code_key                  varchar(32)   null,
-    code                      varchar(64)   null,
+    request_headers           varchar(512)  null,
+    post_fields               varchar(512)  null,
     tokenizer                 varchar(32)   null,
     context                   int           null,
-    parameter_id              int           null,
     currency_id               int           null,
     sent_token_cost           double        null,
     sent_token_audio_cost     double        null,
