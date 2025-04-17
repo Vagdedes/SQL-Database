4774731745a1c95f933b82624c5bce8f49539435
Evangelos <vaggdedes@gmail.com>
2025-04-17 23:50:30 +0300

    Update executableTasks.sql

--- php_async/executableTasks.sql
+++ php_async/executableTasks.sql
@@ -1,9 +1,18 @@
 create table executableTasks
 (
-    id            bigint    not null
+    id                bigint auto_increment
         primary key,
-    creation_date timestamp null,
-    php_code      longtext  null
+    method            varchar(1024) null,
+    method_name       varchar(1024) null,
+    method_parameters longtext      null,
+    code_dependencies longtext      null,
+    debug_code        tinyint(1)    null,
+    debug_result      longtext      null,
+    creation_date     timestamp     null,
+    expiration_date   timestamp     null
 );
+
+create index get
+    on executableTasks (debug_result(768));
 
 
