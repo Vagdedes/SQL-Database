6d67a477bce977255e3123c88cc5d063255e2661
Evangelos <vaggdedes@gmail.com>
2025-07-13 17:36:34 +0300

    Update sqlErrors.sql

--- logs/sqlErrors.sql
+++ logs/sqlErrors.sql
@@ -2,20 +2,11 @@ create table sqlErrors
 (
     id       int auto_increment
         primary key,
-    creation bigint       null,
-    file     varchar(256) null,
-    query    text         null,
-    error    text         null,
-    fixed    datetime     null
+    creation bigint        null,
+    file     varchar(1024) null,
+    query    longtext      null,
+    error    longtext      null,
+    fixed    datetime      null
 );
-
-create index `both`
-    on sqlErrors (creation, file);
-
-create index class
-    on sqlErrors (file);
-
-create index creation
-    on sqlErrors (creation);
 
 
