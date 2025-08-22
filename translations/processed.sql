b917e647621f085fcf0db9f3ea31dd972aeea40e
Evangelos <vaggdedes@gmail.com>
2025-08-22 07:09:42 +0300

    Update processed.sql

--- translations/processed.sql
+++ translations/processed.sql
@@ -1,17 +1,17 @@
 create table processed
 (
-    id               int auto_increment
+    id                   int auto_increment
         primary key,
-    translation_hash bigint        null,
-    language         varchar(64)   null,
-    actual           varchar(8000) null,
-    translation      varchar(8000) null,
-    creation_date    timestamp     null,
-    expiration_date  timestamp     null,
-    deletion_date    timestamp     null
+    translation_hash     bigint      null,
+    translation_language varchar(64) null,
+    actual               longtext    null,
+    translation          longtext    null,
+    creation_date        timestamp   null,
+    expiration_date      timestamp   null,
+    deletion_date        timestamp   null
 );
 
 create index get
-    on processed (translation_hash, language, deletion_date, expiration_date, id);
+    on processed (translation_hash, translation_language, deletion_date, expiration_date, id);
 
 
