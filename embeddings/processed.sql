80fcc3bc972479f68efc5d0531d407df992b586a
Evangelos <vaggdedes@gmail.com>
2025-09-27 23:17:47 +0300

    Create processed.sql

---
+++ embeddings/processed.sql
@@ -0,0 +1,17 @@
+create table processed
+(
+    id              int auto_increment
+        primary key,
+    embedding_hash  bigint       null,
+    embedding_model varchar(128) null,
+    actual          longtext     null,
+    objectified     longtext     null,
+    creation_date   timestamp    null,
+    expiration_date timestamp    null,
+    deletion_date   timestamp    null
+);
+
+create index get
+    on processed (embedding_hash, embedding_model, deletion_date, expiration_date, id);
+
+
