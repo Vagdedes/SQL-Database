74f9dd5254d695fd69dbf35a7928bfa8b0b3d17d
Evangelos <vaggdedes@gmail.com>
2025-12-23 05:52:38 +0200

    updated lengths

--- paypal/successfulTransactions.sql
+++ paypal/successfulTransactions.sql
@@ -2,9 +2,9 @@ create table successfulTransactions
 (
     id             bigint auto_increment
         primary key,
-    transaction_id varchar(64)   null,
-    creation_date  datetime      null,
-    details        varchar(3072) null
+    transaction_id varchar(64) null,
+    creation_date  datetime    null,
+    details        longtext    null
 );
 
 create index `all`
@@ -12,5 +12,8 @@ create index `all`
 
 create index data_pair
     on successfulTransactions (id, details(512));
+
+create index data_pair_date
+    on successfulTransactions (id, details(512), creation_date);
 
 
--- stripe/successfulTransactions.sql
+++ stripe/successfulTransactions.sql
@@ -2,9 +2,9 @@ create table successfulTransactions
 (
     id             bigint auto_increment
         primary key,
-    transaction_id varchar(64)   null,
-    creation_date  datetime      null,
-    details        varchar(3072) null,
+    transaction_id varchar(64) null,
+    creation_date  datetime    null,
+    details        longtext    null,
     constraint id
         unique (id, transaction_id, details(512))
 );
