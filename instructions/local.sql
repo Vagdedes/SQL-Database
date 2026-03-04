2ceab8076f8acbd0df29533abf0020c4931d645c
Evangelos <vaggdedes@gmail.com>
2026-03-05 00:23:20 +0200

    updated old tables

--- instructions/local.sql
+++ instructions/local.sql
@@ -2,19 +2,15 @@ create table local
 (
     id                int auto_increment
         primary key,
-    priority          int           null,
-    information       varchar(4000) null,
-    contains          varchar(4000) null,
-    auto_contains     tinyint(1)    null,
-    browse            tinyint(1)    null,
-    `replace`         tinyint(1)    null,
-    default_use       tinyint(1)    null,
-    creation_date     datetime      null,
-    creation_reason   varchar(2000) null,
-    expiration_date   datetime      null,
-    expiration_reason varchar(2000) null,
-    deletion_date     datetime      null,
-    deletion_reason   varchar(2000) null
+    priority          int            null,
+    information       varchar(15000) null,
+    `replace`         tinyint(1)     null,
+    creation_date     datetime       null,
+    creation_reason   varchar(256)   null,
+    expiration_date   datetime       null,
+    expiration_reason varchar(256)   null,
+    deletion_date     datetime       null,
+    deletion_reason   varchar(256)   null
 );
 
 create index get
