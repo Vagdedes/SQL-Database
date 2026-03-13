842fca95874ff65e8d0f480e654906be0ef82521
Evangelos <vaggdedes@gmail.com>
2026-03-13 16:06:25 +0200

    Update processed.sql

--- embeddings/processed.sql
+++ embeddings/processed.sql
@@ -1,15 +1,16 @@
 create table processed
 (
-    id              int auto_increment
+    id                 int auto_increment
         primary key,
-    embedding_hash  bigint       null,
-    embedding_model varchar(128) null,
-    is_multiple     tinyint(1)   null,
-    actual          longtext     null,
-    objectified     longtext     null,
-    creation_date   timestamp    null,
-    expiration_date timestamp    null,
-    deletion_date   timestamp    null
+    embedding_hash     bigint          null,
+    embedding_model    varchar(128)    null,
+    is_multiple        tinyint(1)      null,
+    actual             longtext        null,
+    objectified        longtext        null,
+    objectified_single varbinary(6144) null,
+    creation_date      timestamp       null,
+    expiration_date    timestamp       null,
+    deletion_date      timestamp       null
 );
 
 create index get
